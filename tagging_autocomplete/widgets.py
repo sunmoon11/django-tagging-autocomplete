52f163b41c1650b94dbe60266080fec3585c1c8e
Han Victor <sunmoon11@yandex.ru>
2018-05-26 22:15:08 +0800

    Fixed for Django 2.0 (django.urls)

--- tagging_autocomplete/widgets.py
+++ tagging_autocomplete/widgets.py
@@ -1,5 +1,5 @@
 from django.forms.widgets import Input
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.conf import settings
 from django.utils.safestring import mark_safe
 
